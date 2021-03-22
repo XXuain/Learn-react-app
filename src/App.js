a823b13cdf612aa359e02c57c5d7988a0af0e1af
yellowHuang <hlady1115@gmail.com>
2021-03-22 17:29:35 +0800

    add simple title component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Title from './components/Title';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    let todos = ['買牛奶', '繳電話費', '去銀行'];
+    this.state = {
+      todos: todos,
+    }
+  }
+
+  createTodo(newtodo) {
+    this.setState({
+      todos: [...this.state.todos, newtodo]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <Title todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;

6e45695a89807464d8d7c53e474a240d7de49ea1
yellowHuang <hlady1115@gmail.com>
2021-03-25 16:23:04 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -1,88 +1,125 @@
 import React, { Component } from 'react';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import AllGroupsTabContent from './components/AllGroupsTabContent';
 import GroupTabContent from './components/GroupTabContent';
 import GroupSwitch from './components/GroupSwitch';
 
 class App extends Component {
+
   constructor() {
     super();
-    // 代辦事項清單
-    let list = [
-      { text: '買牛奶', groupId: 'living' },
-      { text: '繳電話費', groupId: 'living'},
-      { text: '去銀行', groupId: 'living'},
-      { text: '會議', groupId: 'work'},
-      { text: '拜訪客戶', groupId: 'work'},
-      { text: '家長會', groupId: 'family'},
-      { text: '家長會', groupId: 'family'},
+    // 待辦事項清單
+    let todos = [
+      { text: '買牛奶', groupid: 'living' },
+      { text: '繳電話費', groupid: 'living' },
+      { text: '去銀行', groupid: 'living' },
+      { text: '內部會議', groupid: 'work' },
+      { text: '回信', groupid: 'work' },
+      { text: '拜訪客戶', groupid: 'work' },
+      { text: '家長會', groupid: 'family' },
     ];
-    // 代辦事項清單
-    const groups = [
+    // 待辦事項分類
+    let groups = [
       { name: '全部', id: undefined },
       { name: '生活', id: 'living' },
       { name: '工作', id: 'work' },
-      { name: '家庭', id: 'family' },
+      { name: '家庭', id: 'family' }
     ];
+
     this.state = {
-      todos: list,
-      groups,
-      activeGroupId: undefined, // 目前選取的分類
+      todos: todos,
+      groups: groups,
+      // 目前選取中的分類
+      activeGroupId: undefined,
     }
   }
-  createTodo(newtodotext, groupId) {
-    const newTodo = {
+
+  createTodo(newtodotext, groupid) {
+
+    const newtodo = {
       text: newtodotext,
-      groupId: groupId || this.state.activeGroupId
+      groupid: groupid || this.state.activeGroupId
     }
+
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newtodo]
     })
   }
+
   removeTodo(removingtodo) {
-    const newTodos = this.state.todos.filter((todo, index)=> {
-      return !(todo.groupId == removingtodo.groupId && 
+    const newtodos = this.state.todos.filter((todo, index) => {
+      return !(todo.groupid == removingtodo.groupid &&
         removingtodo.text === todo.text)
     });
     this.setState({
-      todos: newTodos
+      todos: newtodos
     })
   }
-  switchGroup(groupId) {
+
+  switchGroup(groupid) {
     this.setState({
-      activeGroupId: groupId
+      activeGroupId: groupid
     })
   }
+
 
   render() {
-    const todos = this.state.activeGroupId ? this.state.todos.filter((todo, index) => {
-      return todo.groupid === this.state.activeGroupId;
-    }) : this.state.todos;
-
-    // 若有選取分類渠組，則顯示群組分類元件，若沒有選取，則顯示全部待辦事項分類
-    const tabContent = this.state.activeGroupId ? 
-      (<GroupTabContent 
-        todos={todos} 
-        createTodo={(newtodo, groupid) => this.createTodo(newtodo, groupid)} 
-        removeTodo={(todo) => this.removeTodo(todo)} 
-      />) 
-      : 
-      (<AllGroupsTabContent 
-        todos={todos} 
-        removeTodo={(todo) => this.removeTodo(todo)} 
-      />);
 
     return (
-      <div>
-        <GroupSwitch
-          groups={this.state.groups}
-          activeGroupId={this.state.activeGroupId}
-          switchGroup={(groupId) => this.switchGroup(groupId)}
-        />  
+      <Router>
         <div>
-          {tabContent}
+          <Route exact path="/"
+            render={() => {
+              return (
+                <div>
+                  <GroupSwitch
+                    groups={this.state.groups}
+                    activeGroupId={undefined}
+                    switchGroup={(groupid) => this.switchGroup(groupid)}
+                  />
+                  <AllGroupsTabContent
+                    todos={this.state.todos}
+                    removeTodo={(todo) => this.removeTodo(todo)}
+                  />
+                </div>
+              );
+
+            }}
+          />
+
+
+          <Route exact path="/group/:id"
+            render={({ match }) => {
+              const todos = this.state.todos.filter((todo, index) => {
+                return todo.groupid === match.params.id;
+              })
+              return (
+                <div>
+                  <GroupSwitch
+                    groups={this.state.groups}
+                    activeGroupId={match.params.id}
+                    switchGroup={(groupid) => this.switchGroup(groupid)}
+                  />
+                  <GroupTabContent
+                    todos={todos}
+                    createTodo={(newtodo, groupid) => this.createTodo(newtodo, groupid)}
+                    removeTodo={(todo) => this.removeTodo(todo)}
+                  />
+                </div>
+              );
+
+            }}
+          />
+
         </div>
-      </div>
+
+      </Router>
     );
   }
 }
--- src/components/GroupSwitch.js
+++ src/components/GroupSwitch.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Clickable } from './StyledComponents'
 import styled from 'styled-components';
+import {
+  Link
+} from 'react-router-dom'
 
 // 群組分類按鈕，繼承自Clickable，並依據是否為目前選取群組改變背景色
 const GroupButton = Clickable.extend`
     border: 1px solid black;
-    background-color: ${props => props.active?'darkorange':'brown'};
+    background-color: ${props => props.active ? 'darkorange' : 'brown'};
     margin: 3px; 
     padding: 3px;
 `;
@@ -16,16 +19,20 @@ class GroupSwitch extends Component {
   render() {
     // 繪製所有群組按鈕
     const groupNode = this.props.groups.map((group, index) => {
-        return (
-            <GroupButton 
-                key={index} 
-                onClick={(e) => this.props.switchGroup(group.id)}
-                active={this.props.activeGroupId === group.id}
-            >
-                    {group.name}
-            </GroupButton>
-        );
-      });      
+      return (
+        <Link key={index} to={group.id ? '/group/' + group.id : '/'}>
+          <GroupButton
+            key={index}
+            onClick={(e) => this.props.switchGroup(group.id)}
+            active={this.props.activeGroupId === group.id}
+          >
+
+            {group.name}
+
+          </GroupButton>
+        </Link>
+      );
+    });
     return <div> {groupNode} </div>
   }
 }

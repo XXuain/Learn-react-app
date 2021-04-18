180a4c2a65b0bebcbc62af154cd71869760613c9
yellowHuang <hlady1115@gmail.com>
2021-04-18 23:13:36 +0800

    try: error handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 
+const ErrorLogLink = onError(({ graphQLErrors, networkError }) => {
+  // console.log(error.networkError.statusCode);
+  if (graphQLErrors) console.log('graphQLErrors: ', graphQLErrors);
+  graphQLErrors.map(({ message, locations, path }) =>
+    console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+  );
+  if (networkError) {
+    console.log('networkError: ', networkError.statusCode);
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+//
+const RequestLink = new HttpLink({
+  // uri: process.env.REACT_APP_APOLLO_SERVER_URL,
+  uri: 'https://plp0mopxq.sse.codesandbox.io/'
+});
+
 // server 連線
 const client = new ApolloClient({
-  uri: 'https://plp0mopxq.sse.codesandbox.io/',
+  link: from([ErrorLogLink, RequestLink]),
   cache: new InMemoryCache()
 });
 
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,11 +3,19 @@ import { useMutation, useQuery } from '@apollo/client';
 import { GET_TODOS, UPDATE_TODO } from '../graphql/todos';
 
 function Todos() {
-  const { loading: queryLoading, error: queryError, data } = useQuery(GET_TODOS);
+  //networkStatus
+  const { loading: queryLoading, error: queryError, data } = useQuery(GET_TODOS, {
+    errorPolicy: 'all'
+  });
   const [updateTodo, { loading: mutationLoading, error: mutationError }] = useMutation(UPDATE_TODO);
-
+  // console.log('networkStatus: ', networkStatus);
   if (queryLoading) return <p>Loading...</p>;
-  if (queryError) return <p>Error :(</p>;
+  // if (queryError) return <p>Error :(</p>;
+  if (queryError) {
+    // console.log('queryError: ', queryError.networkError);
+    // console.log('queryError: ', queryError.graphQLErrors);
+    return <p>Error :(</p>;
+  }
 
   return data.todos.map(({ id, type }) => {
     let input;

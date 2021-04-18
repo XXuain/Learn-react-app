e0865d788c130b5bcf47de3a59e1493eab2cbad6
yellowHuang <hlady1115@gmail.com>
2021-04-18 14:43:29 +0800

    change: install graphQL

--- src/components/CreateTodoButton.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class CreateTodoButton extends Component {
-  render() {
-      return <button onClick={this.props.createTodo}>新增</button>;
-  }
-}
-
-export default CreateTodoButton;

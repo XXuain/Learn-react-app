bff48b9436d5211ce40f0fc617ff2a198d7e0d5c
yellowHuang <hlady1115@gmail.com>
2021-04-18 15:45:20 +0800

    add: 拆出graphQL

---
+++ src/graphql/todos.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+export const GET_TODOS = gql`
+  {
+    todos {
+      id
+      type
+    }
+  }
+`;
+
+export const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+export const ADD_TODO = gql`
+  mutation AddTodo($type: String!) {
+    addTodo(type: $type) {
+      id
+      type
+    }
+  }
+`;

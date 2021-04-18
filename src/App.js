4c894bd2c54dbb74c7892f01a9f66dffb0b637d5
yellowHuang <hlady1115@gmail.com>
2021-04-18 15:46:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+// server 連線
+const client = new ApolloClient({
+  uri: 'https://plp0mopxq.sse.codesandbox.io/',
+  cache: new InMemoryCache()
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>Building Mutation components 🚀</h2>
+      <AddTodo />
+      <Todos />
     </div>
-  );
-}
+  </ApolloProvider>
+);
 
 export default App;

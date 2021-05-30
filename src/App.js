5eec47a15bdb22c533471960ba16778a9e254266
yellowHuang <hlady1115@gmail.com>
2021-05-30 12:58:43 +0800

    delete: old demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import CusApolloProvider from './plugin/apollo';
-import TodoList from './view/TodoList';
-import Context from './view/Context';
-import Reducer from './view/Reducer';
-import Counter from './view/Counter';
 import Hook from './view/Hook';
 
 import { Provider as ReduxProvider } from 'react-redux';
@@ -14,10 +10,6 @@ const store = configureAppStore();
 const App = () => (
   <ReduxProvider store={store}>
     <CusApolloProvider>
-      <TodoList />
-      <Context />
-      <Reducer />
-      <Counter />
       <Hook />
     </CusApolloProvider>
   </ReduxProvider>

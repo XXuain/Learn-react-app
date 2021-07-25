e06dd60994dbb9f0c936fb9675e66ab937170bbd
yellowHuang <hlady1115@gmail.com>
2021-07-25 15:27:42 +0800

    add: lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import CusApolloProvider from './plugin/apollo';
 import 'antd/dist/antd.css';
 
 import MainLayout from './layout/MainLayout';
-
-import Welcome from './view/Welcome';
-import HookTry from './view/HookTry';
-import RouterTry from './view/RouterTry';
+// import Welcome from './view/Welcome';
+// import HookTry from './view/HookTry';
+// import RouterTry from './view/RouterTry';
 
 import configureAppStore from './store';
+
+const Welcome = React.lazy(() => import('./view/Welcome'));
+const HookTry = React.lazy(() => import('./view/HookTry'));
+const RouterTry = React.lazy(() => import('./view/RouterTry'));
 const store = configureAppStore();
 
 const App = () => (
@@ -18,20 +21,22 @@ const App = () => (
     <CusApolloProvider>
       <BrowserRouter>
         <MainLayout>
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/welcome" />
-            </Route>
-            <Route path="/welcome" exact>
-              <Welcome />
-            </Route>
-            <Route path="/routerTry">
-              <RouterTry />
-            </Route>
-            <Route path="/hookTry">
-              <HookTry />
-            </Route>
-          </Switch>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Switch>
+              <Route path="/" exact>
+                <Redirect to="/welcome" />
+              </Route>
+              <Route path="/welcome" exact>
+                <Welcome />
+              </Route>
+              <Route path="/routerTry">
+                <RouterTry />
+              </Route>
+              <Route path="/hookTry">
+                <HookTry />
+              </Route>
+            </Switch>
+          </Suspense>
         </MainLayout>
       </BrowserRouter>
     </CusApolloProvider>

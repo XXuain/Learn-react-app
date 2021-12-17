cda4c66789d742a87e5ffaeed85f054b60d3cfe3
XXuain <hlady1115@gmail.com>
2021-12-17 11:39:38 +0800

    add: retryFun

--- src/App.js
+++ src/App.js
@@ -6,10 +6,38 @@ import 'antd/dist/antd.css';
 import MainLayout from './layout/MainLayout';
 import configureAppStore from './store';
 
-const Welcome = React.lazy(() => import('./view/Welcome'));
-const HookTry = React.lazy(() => import('./view/HookTry'));
-const RouterTry = React.lazy(() => import('./view/RouterTry'));
-const RenderTry = React.lazy(() => import('./view/RenderTry'));
+/**
+ * 解決 Loading chunk failed
+ */
+const retry = (fn, retriesLeft = 5, interval = 1000) => {
+  return new Promise((resolve, reject) => {
+    fn()
+      .then(resolve)
+      .catch((error) => {
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            // reject('maximum retries exceeded');
+            reject(error);
+            return;
+          }
+
+          // Passing on "reject" is the important part
+          retry(fn, retriesLeft - 1, interval).then(resolve, reject);
+        }, interval);
+      });
+  });
+};
+
+const lazyLoadView = (view) => {
+  const LazyView = React.lazy(() => retry(() => import(/* webpackChunkName: "view-[request]" */ `./view/${view}`)));
+  return <LazyView />;
+};
+
+// const Welcome = React.lazy(() => import('./view/Welcome'));
+// const HookTry = React.lazy(() => import('./view/HookTry'));
+// const RouterTry = React.lazy(() => import('./view/RouterTry'));
+// const RenderTry = React.lazy(() => import('./view/RenderTry'));
+
 const store = configureAppStore();
 
 const App = () => (
@@ -23,17 +51,11 @@ const App = () => (
                 <Redirect to="/welcome" />
               </Route>
               <Route path="/welcome" exact>
-                <Welcome />
+                {lazyLoadView('Welcome')}
               </Route>
-              <Route path="/routerTry">
-                <RouterTry />
-              </Route>
-              <Route path="/hookTry">
-                <HookTry />
-              </Route>
-              <Route path="/renderTry">
-                <RenderTry />
-              </Route>
+              <Route path="/routerTry">{lazyLoadView('RouterTry')}</Route>
+              <Route path="/hookTry">{lazyLoadView('HookTry')}</Route>
+              <Route path="/renderTry">{lazyLoadView('RenderTry')}</Route>
             </Switch>
           </Suspense>
         </MainLayout>
